7c846dd28f9f0d7562fe59089eec51f5f81d098f
deeppaz <deeppaz@hotmail.com>
2020-08-07 00:29:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.scss';
 
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { softShadows, MeshWobbleMaterial, OrbitControls } from 'drei';
+
+import { useSpring, a } from 'react-spring/three';
+
+softShadows();
+
+const SpiningMesh = ({ position, args, color, speed }) => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  const [expand, setExpand] = useState(false);
+
+  const props = useSpring({
+    scale: expand ? [1.4,1.4,1.4] : [1, 1, 1],
+  });
+
+  return (
+    <a.mesh onClick={() => setExpand(!expand)} scale={props.scale} castShadow position={position} ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <MeshWobbleMaterial attach="material" color={color} speed={speed} factor={0.6} />
+    </a.mesh>
+  )
+}
+
 
 function App() {
+
   return (
     <>
-      <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometery" args={[1, 1, 1]} />
-          
-        </mesh>
+      <Canvas shadowMap colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-shadowMapWidth={1024}
+          shadow-mapSize-shadowMapHeight={1024}
+          shadowCameraTop={10}
+          shadowCameraLeft={-10}
+          shadowCameraRight={10}
+          shadowCameraBottom={-10}
+          shadowCameraFar={50}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+            {/* <meshStandardMaterial attach="material" color={"yellow"} /> */}
+          </mesh>
+          <SpiningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" speed={2} />
+          <SpiningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+          <SpiningMesh position={[5, 1, -2]} color="pink" speed={6} />
+        </group>
+        <OrbitControls />
       </Canvas>
     </>
   );
