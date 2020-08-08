1ce53130b538889b73f717051f275a3481a00f37
deeppaz <deeppaz@hotmail.com>
2020-08-08 17:57:48 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,67 +1,55 @@
-import React, { useRef, useState } from 'react';
-import './App.scss';
+import React, { Suspense} from 'react'
+import { Canvas } from 'react-three-fiber'
+import { OrbitControls } from 'drei'
 
-import { Canvas, useFrame } from 'react-three-fiber';
-import { softShadows, MeshWobbleMaterial, OrbitControls } from 'drei';
-
-import { useSpring, a } from 'react-spring/three';
-
-softShadows();
-
-const SpiningMesh = ({ position, args, color, speed }) => {
-  const mesh = useRef(null);
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-
-  const [expand, setExpand] = useState(false);
-
-  const props = useSpring({
-    scale: expand ? [1.4,1.4,1.4] : [1, 1, 1],
-  });
-
-  return (
-    <a.mesh onClick={() => setExpand(!expand)} scale={props.scale} castShadow position={position} ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={args} />
-      <MeshWobbleMaterial attach="material" color={color} speed={speed} factor={0.6} />
-    </a.mesh>
-  )
-}
+//components
+import Model from './components/Model/Model'
+import Loading from './components/Loadings/Loading'
 
 
-function App() {
-
+export default function App() {
   return (
     <>
-      <Canvas shadowMap colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
-        <ambientLight intensity={0.3} />
-        <directionalLight
+      <div className="bg" />
+      <h1>
+        I AM
+        <br />
+        <span>ALAN WALKER</span>
+      </h1>
+      <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
+        <ambientLight intensity={0.75} />
+        <pointLight intensity={1} position={[-10, -25, -10]} />
+        <spotLight
           castShadow
-          position={[0, 10, 0]}
-          intensity={1.5}
-          shadow-mapSize-shadowMapWidth={1024}
-          shadow-mapSize-shadowMapHeight={1024}
-          shadowCameraTop={10}
-          shadowCameraLeft={-10}
-          shadowCameraRight={10}
-          shadowCameraBottom={-10}
-          shadowCameraFar={50}
+          intensity={2.25}
+          angle={0.2}
+          penumbra={1}
+          position={[25, 25, 25]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-bias={-0.0001}
         />
-        <pointLight position={[-10, 0, -20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1.5} />
-
-        <group>
-          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach="material" opacity={0.3} />
-            {/* <meshStandardMaterial attach="material" color={"yellow"} /> */}
-          </mesh>
-          <SpiningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" speed={2} />
-          <SpiningMesh position={[-2, 1, -5]} color="pink" speed={6} />
-          <SpiningMesh position={[5, 1, -2]} color="pink" speed={6} />
-        </group>
-        <OrbitControls />
+        <fog attach="fog" args={['#cc7b32', 16, 20]} />
+        <Suspense fallback={null}>
+          <Model url="/scene-draco.gltf" />
+        </Suspense>
+        {/* control of rotate */}
+        <OrbitControls
+          autoRotate
+          enablePan={false}
+          enableZoom={false}
+          enableDamping
+          dampingFactor={0.5}
+          rotateSpeed={1}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
       </Canvas>
+      <div className="layer" />
+      <Loading />
+      <a href="https://github.com/deeppaz/iamalanwalker" className="top-left" children="Github" />
+      {/* <a href="https://twitter.com/0xca0a" className="top-right" children="Twitter" />
+      <a href="https://github.com/drcmda/react-three-fiber" className="bottom-left" children="+ react-three-fiber" /> */}
     </>
-  );
+  )
 }
-
-export default App;
