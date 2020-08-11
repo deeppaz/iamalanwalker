23cecac8c7f4e3e08ecc779c2c2766f35ca9be9a
deeppaz <deeppaz@hotmail.com>
2020-08-11 14:36:37 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense} from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from 'react-three-fiber'
 import { OrbitControls } from 'drei'
 
@@ -16,19 +16,21 @@ export default function App() {
         <span>ALAN WALKER</span>
       </h1>
       <Canvas shadowMap camera={{ position: [-10, -10, 15] }}>
-        <ambientLight intensity={0.75} />
+        <pointLight position={[0, 60, -100]} intensity={10} />
+        <pointLight position={[-50, 0, -50]} intensity={2} />
+        <ambientLight intensity={5} />
         <pointLight intensity={1} position={[-10, -25, -10]} />
         <spotLight
           castShadow
-          intensity={2.25}
-          angle={0.2}
+          intensity={5}
+          angle={4}
           penumbra={1}
-          position={[25, 25, 25]}
+          position={[50, 50, 50]}
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
           shadow-bias={-0.0001}
         />
-        {/* <fog attach="fog" args={['#cc7b32', 16, 20]} /> */}
+        <fog attach="fog" args={['#cc7b32', 16, 20]} />
         <Suspense fallback={null}>
           <Model url="scene-draco.gltf" />
         </Suspense>
--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -7,10 +7,9 @@ import { draco } from 'drei'
 export default function Model({ url }) {
   const { nodes, materials } = useLoader(GLTFLoader, url, draco())
   return (
-    <group rotation={[-Math.PI / 1, 30, 0]} position={[-15, -3, 0]} scale={[6, 6, 6]}>
-      <group rotation={[Math.PI / 2, 0, 0]} scale={[0.01, 0.01, 0.01]}>
-        <mesh castShadow receiveShadow geometry={nodes.Drone_Guns_0.geometry} material={materials.Bilbord_Material__36} />
-        <mesh castShadow receiveShadow geometry={nodes.Drone_Guns_1.geometry} material={materials.Bilbord_Material__37} />
+    <group rotation={[-Math.PI / 1, 0, 0]} position={[1, -10, 0]} scale={[6, 6, 6]}>
+      <group rotation={[Math.PI / 2, 0, 0]} scale={[1.5, 1.5, 1.5]}>
+        <mesh castShadow receiveShadow geometry={nodes.Drone_0.geometry} material={materials.None} />
       </group>
     </group>
   )
